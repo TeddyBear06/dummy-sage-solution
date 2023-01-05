996089a5a1035cf913415ffbcf87a7d90da3ec29
T3d <TeddyBear06@users.noreply.github.com>
2023-01-05 02:26:34 +0100

    POC
    Proves that main application's manifest impact ActiveX library's version.

---
+++ ConsoleApp1/DummySageLibrary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Objets100cLib;
+
+namespace DummySageLibrary
+{
+    public class DummySageLibrary
+    {
+        public string GetOMVersion()
+        {
+            try
+            {
+                BSCPTAApplication100c om_baseCial = new BSCPTAApplication100c();
+                return "OM version : " + om_baseCial.Licence.Version;
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+                return e.Message;
+            }
+        }
+    }
+}
